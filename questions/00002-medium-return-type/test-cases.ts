7d8748b9f3d1689183ae70a54efa1a1f9fd56d1c
yanzhuang <canvascat@qq.com>
2022-07-31 23:43:19 +0800

    do: MyReturnType

--- questions/00002-medium-return-type/test-cases.ts
+++ questions/00002-medium-return-type/test-cases.ts
@@ -18,3 +18,5 @@ type ComplexObject = {
 
 const fn = (v: boolean) => v ? 1 : 2
 const fn1 = (v: boolean, w: any) => v ? 1 : 2
+
+type MyReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => infer R ? R : never
