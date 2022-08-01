dcc4b45136e997c9841a8c42b5452caf1b5bbc84
yanzhuang <canvascat@qq.com>
2022-08-02 00:33:04 +0800

    infer

--- questions/00015-medium-last/test-cases.ts
+++ questions/00015-medium-last/test-cases.ts
@@ -4,3 +4,5 @@ type cases = [
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
   Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
 ]
+
+type Last<T extends readonly any[]> = T extends [...infer _R, infer Last] ? Last : never
--- questions/00016-medium-pop/test-cases.ts
+++ questions/00016-medium-pop/test-cases.ts
@@ -4,3 +4,5 @@ type cases = [
   Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
   Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
 ]
+
+type Pop<T extends readonly any[]> = T extends [...infer Rest, infer _Last] ? Rest : never
